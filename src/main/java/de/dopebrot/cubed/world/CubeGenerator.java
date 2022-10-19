9b6d347aeab4061bef482e28ba6257c2605955e0
Hptlo <niclaselling8@gmail.com>
2022-10-19 10:57:08 +0200

    changed hashmap input "fixed" override of cube

--- src/main/java/de/dopebrot/cubed/world/CubeGenerator.java
+++ src/main/java/de/dopebrot/cubed/world/CubeGenerator.java
@@ -2,31 +2,37 @@ package de.dopebrot.cubed.world;
 import de.dopebrot.cubed.Main;
 import de.dopebrot.cubed.cubes.Cube;
 import org.apache.commons.lang.Validate;
-import org.bukkit.Chunk;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
 public class CubeGenerator {
 
-	private Main main;
+	private final Main main;
 	private final ArrayList<Cube> cubeArrayList;
+	private final HashMap<Player, Cube> cubeHashMap;
 	private World world;
+	private final Random random;
 
 	public CubeGenerator(Main main) {
 		this.main = main;
 		this.cubeArrayList = new ArrayList<>();
+		this.cubeHashMap = new HashMap<>();
+		this.random = new Random();
 	}
-
+	public World getWorld() {
+		return world;
+	}
 	public void setWorld(World world) {
 		this.world = world;
 	}
+
 	/**
 	 * for every 100 cubes a new cube line generates
 	 */
-
 	public void generateNew(Player player) {
 		Validate.notNull(player, "player can't be null");
 		for (Cube c : cubeArrayList) {
@@ -40,17 +46,26 @@ public class CubeGenerator {
 							}
 						}
 					}
-					cubeArrayList.remove(c);
+					// override old one
+					Cube n = new Cube(player.getUniqueId(), 9000, 99);
+					n.setChunk(world.getChunkAt(100 - cubeArrayList.size() + 1, 100 - ((int) (cubeArrayList.size() * 0.01) + 1)));
+					n.clearCube(true);
+					n.setBlocks();
+					n.clearCube(false);
+					cubeArrayList.set(cubeArrayList.indexOf(c), n);
+					cubeHashMap.put(player, n);
+					return;
 				}
 			}
 		}
 
-		Cube n = new Cube(player.getUniqueId(), 9000, 5);
+		Cube n = new Cube(player.getUniqueId(), 9000, 99);
 		n.setChunk(world.getChunkAt(100 - cubeArrayList.size() + 1, 100 - ((int) (cubeArrayList.size() * 0.01) + 1)));
 		n.clearCube(true);
 		n.setBlocks();
 		n.clearCube(false);
 		cubeArrayList.add(n);
+		cubeHashMap.put(player, n);
 	}
 
 	public boolean containsPlayer(Player player) {
@@ -75,6 +90,20 @@ public class CubeGenerator {
 			}
 		}
 		return null;
+	}
+
+	public void deleteCube(Cube cube) {
+		Validate.notNull(cube,"cube can't be null");
+
+		cube.resetCube();
+
+		this.cubeArrayList.remove(cube);
+		OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(cube.getBelongingPlayer());
+		if (offlinePlayer.getPlayer() != null) {
+			this.cubeHashMap.remove(offlinePlayer.getPlayer());
+		}
+
+
 	}
 
 }
