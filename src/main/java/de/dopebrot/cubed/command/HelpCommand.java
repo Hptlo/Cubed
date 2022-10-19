7d0d96e48562728497d9d932ce0524fa35c624ca
Hptlo <niclaselling8@gmail.com>
2022-10-19 10:57:40 +0200

    new version of DopeAPI changed DPCommand

--- src/main/java/de/dopebrot/cubed/command/HelpCommand.java
+++ src/main/java/de/dopebrot/cubed/command/HelpCommand.java
@@ -2,26 +2,22 @@ package de.dopebrot.cubed.command;
 
 import de.dopebrot.dopeapi.helper.DPCommand;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class HelpCommand implements DPCommand {
 
-	@Override
-	public String[] getPermissions() {
-		return new String[0];
-	}
-	@Override
-	public String getCommandName() {
-		return null;
-	}
-	@Override
-	public void onDebug() {
-
-	}
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 		return false;
 	}
 
+	@Override
+	public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+		return null;
+	}
 }
